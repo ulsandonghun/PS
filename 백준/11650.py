bed080229322b8dce4cc9e4cfc190667efaafc47
ulsandonghun <donghoon141@naver.com>
2023-10-10 00:44:41 +0900

    [백준] : 11650 별해 추가

--- 백준/11650.py
+++ 백준/11650.py
@@ -7,8 +7,8 @@ lst=[]
 for _ in range(N):
     a,b=map(int,sys.stdin.readline().strip().split())
     lst.append((a,b))
-lst.sort(key=lambda x : (x[0],x[1]) )
-
+# lst.sort(key=lambda x : (x[0],x[1]) )  이 방법도 가능하고 파이썬은 어짜피순서대로 정렬하기 때문에 그대로 sort 만 써도 됨.
+lst.sort()
 for i in lst:
     print(i[0],i[1])
 
