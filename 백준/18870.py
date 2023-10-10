160d00a2c148dbbb4e6fe8bddfabec5a607996ac
ulsandonghun <donghoon141@naver.com>
2023-10-11 02:37:53 +0900

    [백준] : 18870번 리스트의 검색속ëë를 딕셔너리로 그바꿔서 검색 속도 log(1)로 구현

--- 백준/18870.py
+++ 백준/18870.py
@@ -5,10 +5,20 @@ N= int(sys.stdin.readline())
 lst=[]
 result=[]
 
-cnt=0
-for _ in range(N):
-    lst=list(map(int,sys.stdin.readline().split()))
 
-lst.sort(key = lambda  x :x[0] )
+lst=list(map(int,sys.stdin.readline().split()))
+
+result=sorted(list(set(lst)))
+
+dic={}
+for i in range(len(result)):
+    dic[result[i]]=i #어렵게 생각하지 말고, 리스트에 저장된 것을 그대로 딕셔너리로 옮긴 것임.
+    #딕셔너리는 검색속도가 log 1 이기 때문임.
+
 for i in lst:
-    print()
+    print(dic[i],end=" ") #dictionary를 이용해 검색 속도를 log(1)로 낮춤.
+    # list의 index 검색은 log(N)임.
+
+
+# for i in lst:
+#     print(result.index(i),end=" ")
