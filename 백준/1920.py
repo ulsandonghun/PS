ba683bb31849a974e96f6628b5b1efeffcd683c5
ulsandonghun <donghoon141@naver.com>
2023-10-12 23:58:33 +0900

    [ã백준] : 1920번 문제풀이 딕셔너리로 log(1) 검색 ì복잡도 이용

---
+++ 백준/1920.py
@@ -0,0 +1,22 @@
+import sys
+
+N = int(sys.stdin.readline())
+lst = []
+
+lst=list(map(int,sys.stdin.readline().split()))
+
+dic={}
+for i in len(list):
+    dic[lst[i]]=i
+
+M = int(sys.stdin.readline())
+
+search=[]
+
+search=list(map(int,sys.stdin.readline().split()))
+
+for _ in search:
+    if _ in dic:
+        print(1)
+    else:
+        print(0)
