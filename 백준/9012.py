f23c4cd9dcb66d6bf6b8396ecab214074287541f
ulsandonghun <donghoon141@naver.com>
2023-10-25 01:57:59 +0900

    [백준] : 9012번 함수의 파라미터로 배열을 받아서 스택으로 이용(깊은복사라서 원본 변경)

--- 백준/9012.py
+++ 백준/9012.py
@@ -3,7 +3,33 @@ import sys
 T = int(sys.stdin.readline())
 stack=[]
 
-def boolean judge(str,list)
+def  judge(str,stack=[]):
+
+    arr=[ ]
+    arr=list(str)
+    for i in arr:
+        if len(stack)>0:
+            if(stack[-1]=='('and i==')'):
+                stack.pop()
+            elif( stack[-1]==')'and i==')'):
+
+                stack.append(i)
+                # print("break 수행 전현재 스택의 상태 : ",stack)
+                break
+            else:
+                stack.append(i)
+                # print("현재 스택의 상태 : ",stack)
+        else:
+            stack.append(i)
+
+    if stack:
+        # print("현재 스택의 상태 : ",stack)
+        print("NO")
+    else:
+        # print("현재 스택의 상태 : ",stack)
+        print("YES")
+    stack.clear()
 
 for _ in range(T):
     str=sys.stdin.readline().strip()
+    judge(str,stack)
