d8eb4aec6c73f5efa2c21dfa6d724386e37dcc60
ulsandonghun <donghoon141@naver.com>
2023-10-27 23:24:26 +0900

    [백준] : 2164번 내ì 2164번 시간복잡도 O(1)로 pop 하는 과정 최적화 라이브러리 ãdeque 이용

--- 백준/2164.py
+++ 백준/2164.py
@@ -1,14 +1,17 @@
 import sys
-
+from collections import deque
 N = int(sys.stdin.readline())
+deck=deque([i+1 for i in range(N)])
 
-deck=[i  for i in range(1,N+1)]
+# deck=[i  for i in range(1,N+1)]
 #1에서 N 까지 리스트에 저장
 
 
 while(len(deck)>1):
-    deck.pop(0);
-    # deck.insert(len(deck)-1,deck.pop())
-    deck.append(deck.pop(0))
+    deck.popleft();
+    deck.append(deck.popleft())
+    # deck.pop(0);
+    # deck.insert(len(deck)-1,deck.pop(0))
+    # deck.append(deck.pop(0))
 print(deck.pop())
 
