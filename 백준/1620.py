3cc527ba1d9f77385b62424e1eacff1cae0fdbbf
ulsandonghun <donghoon141@naver.com>
2023-10-31 23:36:22 +0900

    [ë[refactor] : 순회를 돌기 전 key 값을 우선 찾으면 value값을 찾지 않도록 시간복잡도 최소화

--- 백준/1620.py
+++ 백준/1620.py
@@ -10,11 +10,11 @@ for _ in range(N):
 # print(dic)
 for j in range(M):
     word=sys.stdin.readline().strip()
-    for key, value in dic.items():
-        if(word == key):
-            # print("현재"+word+"값이 key 값으로 존재함으로 value 값인 "+dic[word]+"값을 반환합니다.")
-            print(dic[word])
-        elif(word == value):
-            # print("현재"+word+"값이 value 값으로 존재함으로 key 값인 "+key+"값을 반환합니다.")
-            print(key)
+    if(word in dic):
+        print(dic[word])
+    else:
+        for key, value in dic.items():
+            if(value==word):
+                print(key)
+
 
