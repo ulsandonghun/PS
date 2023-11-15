7b3cdb201a0389e47f72305ba1be75ffcbe5b654
ulsandonghun <donghoon141@naver.com>
2023-11-16 08:53:55 +0900

    [백준] : 11399번 한번의 사이클을 돌때마다 더해가는 수를 하나씩 늘리는 구현

--- 백준/11399.py
+++ 백준/11399.py
@@ -3,4 +3,18 @@ import sys
 N= int(sys.stdin.readline())
 p=[]
 
+p=list(map(int,input().split()))
+p.sort()
+result=0
+#한번의 사이클마다 더해가는 수를 하나씩 늘려가야 함--> 이중for문
+for i in range(len(p)):
+    for j in range(0,i+1):
+        result+=p[j]
+print(result)
+
+
+
+
+
+
 
