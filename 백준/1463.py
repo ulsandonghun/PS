82e736d9194d014bcab91157e579684b20212b3d
ulsandonghun <donghoon141@naver.com>
2024-04-20 23:28:27 +0900

    DynamicPrograming -memozation -retry

--- 백준/1463.py
+++ 백준/1463.py
@@ -2,10 +2,14 @@ X = int(input())
 cnt=0
 dp=[0]*(X+1)
 dp[1]=0
-dp[2]=1
 
-for i in range(3,X+1):
-    dp[i]=min(dp[i-1]+1,dp[i//3]+1,dp[i//2]+1)
+for i in range(2,X+1):
+    dp[i]=dp[i-1]+1
+    if i%3==0:
+        dp[i]=min(dp[i],dp[i//3]+1)
+    elif i%2==0:
+        dp[i]=min(dp[i],dp[i//2]+1)
+
 
 print(dp[X])
 
---
+++ 백준/문법연습.py
@@ -0,0 +1,7 @@
+
+a=list([1,2,3,4])
+print(a)
+# d=dict('dh':1)
+d=dict(a)
+
+print(d)
